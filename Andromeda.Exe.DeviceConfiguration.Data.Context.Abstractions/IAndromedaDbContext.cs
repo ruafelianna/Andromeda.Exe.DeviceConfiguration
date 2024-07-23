33f2cc509f80dbb546377c0a5d04d5a3051055a2
Ruafel <ruafelianna@ya.ru>
2024-07-23 15:10:39 +0300

    Add db entities AppInstance and LoginRecord to db context

--- Andromeda.Exe.DeviceConfiguration.Data.Context.Abstractions/IAndromedaDbContext.cs
+++ Andromeda.Exe.DeviceConfiguration.Data.Context.Abstractions/IAndromedaDbContext.cs
@@ -1,8 +1,12 @@
 using Andromeda.Data.Context.Abstractions;
+using Andromeda.Exe.DeviceConfiguration.Data.Models.App;
+using Microsoft.EntityFrameworkCore;
 
 namespace Andromeda.Exe.DeviceConfiguration.Data.Context.Abstractions
 {
     public interface IAndromedaDbContext : IDbContext
     {
+        DbSet<AppInstance> AppInstances { get; }
+        DbSet<LoginRecord> LoginRecords { get; }
     }
 }
--- Andromeda.Exe.DeviceConfiguration.Data.Context/AndromedaDbContextBase.cs
+++ Andromeda.Exe.DeviceConfiguration.Data.Context/AndromedaDbContextBase.cs
@@ -1,5 +1,6 @@
 using Andromeda.Data.Context;
 using Andromeda.Exe.DeviceConfiguration.Data.Context.Abstractions;
+using Andromeda.Exe.DeviceConfiguration.Data.Models.App;
 using Microsoft.EntityFrameworkCore;
 
 namespace Andromeda.Exe.DeviceConfiguration.Data.Context
@@ -14,5 +15,9 @@ namespace Andromeda.Exe.DeviceConfiguration.Data.Context
         public AndromedaDbContextBase(DbContextOptions options) : base(options)
         {
         }
+
+        public DbSet<AppInstance> AppInstances { get; set; }
+
+        public DbSet<LoginRecord> LoginRecords { get; set; }
     }
 }
